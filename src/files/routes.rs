3796c0b8896db4e6929d980e528d42b3f9faa7bb
Nuke <nuke-web3@proton.me>
2025-01-19 18:52:04 -0700

    error flip/flop  https://github.com/tamasfe/aide/blob/7bdfa126f722126ae68d85feddd88a54e2d657f8/crates/aide/src/axum/outputs.rs#L17 not impl for range or https://x3ro.de/axum-the-trait-handler_-is-not-implemented-for-fn-item/ fucking bs

--- src/files/routes.rs
+++ src/files/routes.rs
@@ -7,13 +7,15 @@ use aide::{
 };
 use axum::{
     extract::{DefaultBodyLimit, Multipart, Path},
-    http::{header, HeaderMap, StatusCode},
+    http::StatusCode,
     response::IntoResponse,
 };
+use axum_extra::{headers::Range, TypedHeader};
+use axum_range::{KnownSize, Ranged};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use tokio::fs::File;
-use tokio::io::{AsyncReadExt, AsyncSeekExt, AsyncWriteExt};
+use tokio::io::AsyncWriteExt;
 use uuid::Uuid;
 
 use crate::{errors::AppError, extractors::Json, state::AppState};
@@ -24,7 +26,7 @@ pub fn file_service_routes(state: AppState) -> ApiRouter {
         // FIXME: want a sensible max file upload size! (2 MB is the default https://docs.rs/axum/latest/axum/extract/struct.Multipart.html)
         .layer(DefaultBodyLimit::disable());
     let file_download_route = ApiRouter::new().api_route(
-        "/download/:filename",
+        "/download:filename",
         get_with(download_file, download_file_docs),
     );
     let merged_routes = ApiRouter::new()
@@ -40,9 +42,6 @@ pub fn file_service_routes(state: AppState) -> ApiRouter {
 struct FileWrapper {
     /// The ID of the new file.
     id: Uuid,
-    /// TODO: where to store size info or just read it on neeed?
-    /// want the size? https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Length
-    size: usize,
 }
 
 /// Unfortunately, Aide requires a struct in the form below to properly display
@@ -61,85 +60,76 @@ async fn upload_file(
     // TODO: use a real db backend
     let mut file = File::create(format!("{}/{}", "/tmp", id)).await.unwrap();
 
-    // TODO: get map_err() to work with async instead of match
-    match stream_to_file(multipart, &mut file).await {
-        Ok(_) => println!("💾 Streamed data written to disk"),
-        Err(_) => {
-            println!("❌ Interupted upload! Drop & delete file uploaded...");
+    let _ = stream_to_file(multipart, &mut file)
+        .await
+        .map_err(|_| async {
             // NOTE: https://docs.rs/tokio/latest/tokio/fs/struct.File.html may need to flush... if we care about saving partial uploads
             drop(file);
-            tokio::fs::remove_file(id.to_string()).await.unwrap();
-            return Err(AppError::new("File upload interupted"));
-        }
-    }
+            let file_delete = tokio::fs::remove_file(id.to_string()).await;
+            if let Err(_) = file_delete {
+                return AppError::new("File removal failed! FS unclean!");
+            };
+            return AppError::new("File upload interupted, upload deleted.");
+        });
 
     Ok((StatusCode::CREATED, Json(FileWrapper { id })))
 }
 
 /// Axum service to handle file download requests
 /// FIXME: On integration, we need to do auth via token `Session` extrating on the request
-use axum_macros::debug_handler;
-#[debug_handler]
 async fn download_file(
     // State(app): State<Arc<AppState>>,
     Path(filename): Path<FileIdPath>,
-    headers: HeaderMap,
+    range: Option<TypedHeader<Range>>,
 ) -> Result<impl IntoApiResponse, AppError> {
-    let range = headers.get("range").ok_or(
-        AppError::new("Malformed file download request").with_status(StatusCode::NOT_ACCEPTABLE),
-    )?;
-
-    let filepath = format!("{}/{}", "/tmp", filename.id); // FIXME: use app state to set a file store location on disc
+    // FIXME: use app state to set a file store location on disc
+    let filepath = format!("{}/{}", "/tmp", filename.id);
 
     // FIXME: need to only load the bytes we need from disk to memory, not whole file
     // Ideally get a page that is optimal for any client (is there one?) and/or web
     // See https://github.com/tokio-rs/axum/discussions/1638
     // https://lib.rs/crates/axum-streams
     // how does axum handle a last packet failing? (not mid stream where it can be detected for sure)
-    let mut file = match File::open(&filepath).await {
+    let file = match File::open(&filepath).await {
         Ok(file) => file,
         Err(_) => return Err(AppError::new("File not found").with_status(StatusCode::NOT_FOUND)),
     };
 
-    let file_size = file.metadata().await.unwrap().len();
-    let range_str = range.to_str().unwrap();
-    // TODO: better way to ensure GET headers are correct and make RANGE optional!
-    // FIXME: remove all unwrap!
-    if let Some(range) = range_str.strip_prefix("bytes=") {
-        let (start, end) = range.split_once('-').unwrap();
-        let start: u64 = start.parse().unwrap();
-        let end: u64 = end.parse().unwrap_or(file_size - 1);
+    let body = KnownSize::file(file).await.unwrap();
+    let range = range.map(|TypedHeader(range)| range);
 
-        file.seek(std::io::SeekFrom::Start(start)).await.unwrap();
-        let mut buffer = vec![0; (end - start + 1) as usize];
-        // FIXME: need to handle out of bounds ranges
-        file.read_exact(&mut buffer).await.unwrap();
+    let r = Ranged::new(range, body);
+    r.try_respond().map_err(|_| {
+        AppError::new("Malformed file download request").with_status(StatusCode::NOT_ACCEPTABLE)
+    })
 
-        let mut response = (
-            StatusCode::PARTIAL_CONTENT,
-            [
-                (
-                    header::CONTENT_RANGE,
-                    format!("bytes {}-{}/{}", start, end, file_size),
-                ),
-                (header::CONTENT_LENGTH, (end - start + 1).to_string()),
-                (header::CONTENT_TYPE, "application/octet-stream".to_string()),
-                (
-                    header::CONTENT_DISPOSITION,
-                    format!("attachment; filename=\"{}\"", filename.id),
-                ),
-            ],
-            buffer,
-        )
-            .into_response();
-        response.headers_mut().insert(
-            header::ACCEPT_RANGES,
-            header::HeaderValue::from_static("bytes"),
-        );
-        return Ok(response);
-    }
+    // let mut response = (
+    //     StatusCode::PARTIAL_CONTENT,
+    //     [
+    //         (
+    //             header::CONTENT_RANGE,
+    //             format!("bytes {}-{}/{}", read_range.0, read_range.1, file_size),
+    //         ),
+    //         (
+    //             header::CONTENT_LENGTH,
+    //             (read_range.1 - read_range.0 + 1).to_string(),
+    //         ),
+    //         (header::CONTENT_TYPE, "application/octet-stream".to_string()),
+    //         (
+    //             header::CONTENT_DISPOSITION,
+    //             format!("attachment; filename=\"{}\"", filename.id),
+    //         ),
+    //     ],
+    //     buffer,
+    // )
+    //     .into_response();
+    // response.headers_mut().insert(
+    //     header::ACCEPT_RANGES,
+    //     header::HeaderValue::from_static("bytes"),
+    // );
+    // return Ok(response);
 
-    Err(AppError::new("Malformed file download request").with_status(StatusCode::NOT_ACCEPTABLE))
+    // Err(AppError::new("Malformed file download request").with_status(StatusCode::NOT_ACCEPTABLE))
 }
 
 /// Stream uploaded data to a given file on disk.
